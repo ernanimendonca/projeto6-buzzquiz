a4b3d435b024d3a183650965054efd728a604bda
Hiann <hiannmanso@email.com>
2022-02-18 02:23:14 -0300

    get lista quizzes feito

--- script.js
+++ script.js
@@ -15,3 +15,35 @@ function makeScreenActive(nScreen) {
         }
     }
 }
+getAllQuizzes()
+ function getAllQuizzes() {
+     const URL = 'https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes'
+    axios({
+        method:'get',
+        url: URL,
+    
+    }).then(item=>{
+        console.log(item);
+        renderAllQuizzes(item)
+    }).catch(error => {
+        console.log(error);
+    })
+ }  
+
+ function renderAllQuizzes(response){
+     let container = document.querySelector('.all')
+     container.innerHTML=''
+     for (let index = 0; index <response.data.length; index++) {
+       console.log(response.data[index]);
+         container.innerHTML +=
+         `
+            <div class="quizz" id=${response.data[index].id} onclick="pageQuizz(${response.data[index].id})">
+                <p>${response.data[index].title}</p>
+                <div class="image">
+                    <img src="${response.data[index].image}" alt="Simpsons">
+                    <div class="overlap"></div>
+                </div>
+            </div>
+         `
+     }
+ }
